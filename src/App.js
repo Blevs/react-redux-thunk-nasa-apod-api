f32b4403f90c7787f0b93612b6d1444bf3b67a3e
Henry Blevins <heblevi@gmail.com>
2019-08-15 21:14:52 -0400

    Complete Recitation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import { connect } from 'react-redux';
+import { getApod } from './actions';
 
-function App() {
+function App(props) {
+  console.log(props);
+  const [date, setDate] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Nasa Photo of the Day </h1>
+      <input type="text"
+             palceholder="date"
+             value={date}
+             onChange={e => setDate(e.target.value)}/>
+      {props.loading && <div>loading...</div>}
+      {props.apod && (
+        <div>
+          <img src={props.apod.url}/>
+          <p>{props.apod.explanation}</p>
+        </div>)}
+      {props.error !== "" && <p>{props.error}</p>}
+      <button onClick={() => props.getApod(date)}>Get Photo of the Day</button>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    apod: state.apod,
+    error: state.error,
+    loading: state.loading
+  };
+};
+
+export default connect(mapStateToProps, { getApod })(App);
